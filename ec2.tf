ddcbc6a8f4da3ea39da1e239764619b457baff65
NAZIYASHAIK1695 <101784632+NAZIYASHAIK1695@users.noreply.github.com>
2022-06-10 10:18:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "demoinstance" {
   ami = "ami-0fa49cc9dc8d62c84"
   instance_type = "t2.micro"
   count = 1
-  key_name = "ohio-home"
+  key_name = "tests"
   vpc_security_group_ids = ["${aws_security_group.demosg.id}"]
   subnet_id = "${aws_subnet.public-subnet-1.id}"
   associate_public_ip_address = true
@@ -17,7 +17,7 @@ resource "aws_instance" "demoinstance1" {
   ami = "ami-0fa49cc9dc8d62c84"
   instance_type = "t2.micro"
   count = 1
-  key_name = "ohio-home"
+  key_name = "tests"
   vpc_security_group_ids = ["${aws_security_group.demosg.id}"]
   subnet_id = "${aws_subnet.public-subnet-2.id}"
   associate_public_ip_address = true
