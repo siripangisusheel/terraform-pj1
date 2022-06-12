bd7d438e62f8cc0da4d25ed09b4aedf1c3d0a4de
Amar Mattaparthi <89722343+amar-m-cloud@users.noreply.github.com>
2022-06-12 06:44:45 +0530

    Add files via upload

---
+++ alb.tf
@@ -0,0 +1,45 @@
+resource "aws_lb" "external-elb" {
+  name               = "External-LB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.demosg.id]
+  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+}
+
+resource "aws_lb_target_group" "external-elb" {
+  name     = "ALB-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.demovpc.id
+}
+
+resource "aws_lb_target_group_attachment" "external-elb1" {
+  target_group_arn = aws_lb_target_group.external-elb.arn
+  target_id        = aws_instance.demoinstance.id
+  port             = 80
+
+  depends_on = [
+    aws_instance.demoinstance,
+  ]
+}
+
+resource "aws_lb_target_group_attachment" "external-elb2" {
+  target_group_arn = aws_lb_target_group.external-elb.arn
+  target_id        = aws_instance.demoinstance1.id
+  port             = 80
+
+  depends_on = [
+    aws_instance.demoinstance,
+  ]
+}
+
+resource "aws_lb_listener" "external-elb" {
+  load_balancer_arn = aws_lb.external-elb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.external-elb.arn
+  }
+}
