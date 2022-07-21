29802728f96faca444d33eff6ac3393db9d1717f
siripangisusheel <100437307+siripangisusheel@users.noreply.github.com>
2022-07-21 16:22:42 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 # creating 1st EC2 instance in Public Subnet
 resource "aws_instance" "demoinstance" {
   ami = "ami-0fa49cc9dc8d62c84"
-  instance_type = "t2.2xlarge"
+  instance_type = "t2.micro"
   key_name = "tests"
   vpc_security_group_ids = ["${aws_security_group.demosg.id}"]
   subnet_id = "${aws_subnet.public-subnet-1.id}"
@@ -14,7 +14,7 @@ resource "aws_instance" "demoinstance" {
 # creating 2nd EC2 instance in Public Subnet
 resource "aws_instance" "demoinstance1" {
   ami = "ami-0fa49cc9dc8d62c84"
-  instance_type = "t2.2xlarge"
+  instance_type = "t2.micro"
   key_name = "tests"
   vpc_security_group_ids = ["${aws_security_group.demosg.id}"]
   subnet_id = "${aws_subnet.public-subnet-2.id}"
